6301c8e141334fe22d5b73413b6d05d91f872dd1
17VaibhaVPal <vaibhavpal17sept@gmail.com>
2024-11-11 13:15:30 +0530

    Artists added and modified

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LibraryScreen extends StatelessWidget {
+class LibraryScreen extends StatefulWidget {
+  @override
+  _LibraryScreenState createState() => _LibraryScreenState();
+}
+
+class _LibraryScreenState extends State<LibraryScreen> {
   final List<String> songs = [
-    'Closer',
-    'Baby',
-    'Espresso',
-    'Desi Kalakar',
-    'Zara Sa',
-    'Hale Dil',
-    'Attention',
-    'We Don\'t Talk Anymore',
-    'Harleys in Hawaii',
-    'Fuck Up the Friendship',
-    'Señorita'
+    'Closer', 'Baby', 'Espresso', 'Desi Kalakar', 'Zara Sa',
+    'Hale Dil', 'Attention', 'We Don\'t Talk Anymore', 'Harleys in Hawaii',
+    'Fuck Up the Friendship', 'Señorita'
   ];
 
   final List<String> songImageUrls = [
@@ -29,8 +26,35 @@ class LibraryScreen extends StatelessWidget {
     'https://i.ytimg.com/vi/Pkh8UtuejGw/maxresdefault.jpg'
   ];
 
-  final List<String> albums = ['Album 1', 'Album 2', 'Album 3', 'Album 4'];
-  final List<String> artists = ['Artist 1', 'Artist 2', 'Artist 3', 'Artist 4'];
+  final List<String> albums = [
+    'Aashique 2', 'Jab We Met', 'Ajab Prem Ki Ghazab Kahani', 'Jannat 2', 'Genius', 'Chhichhore'
+  ];
+
+  final List<String> albumImageUrls = [
+    'https://assets-in.bmscdn.com/discovery-catalog/events/et00013287-vnqbwzksbl-landscape.jpg',
+    'https://m.media-amazon.com/images/M/MV5BOTk0NDU0OGEtZTRlMy00ZTZkLWJiYmEtN2QwMzBhNTg2N2I0XkEyXkFqcGc@._V1_.jpg',
+    'https://img.mensxp.com/media/content/2021/Dec/What-Ajab-Prem-Ki-Ghazab-Kahani-Is-Really-About1200_61b205b9c6e8f.jpeg',
+    'https://i.scdn.co/image/ab67616d0000b27371da5e89467bd75d2ed9f1fa',
+    'https://m.media-amazon.com/images/M/MV5BOTM3NzVkZDQtMjgzMC00OTcxLWE1YTEtNjhlNDYyNjg3MGYxXkEyXkFqcGc@._V1_.jpg',
+    'https://images.indianexpress.com/2019/09/chhichhore-movie-review-759.jpg',
+  ];
+
+  final List<String> artists = [
+    'Arijit Singh', 'Honey Singh', 'Krishnakumar Kunnath (KK)', 'Sonu Nigam', 'Sunidhi Chauhan',
+    'Justin Bieber', 'Charlie Puth', 'Shreya Ghoshal'
+  ];
+
+  final List<String> artistImageUrls = [
+    'https://static.toiimg.com/thumb/imgsize-23456,msid-92037952,width-600,resizemode-4/92037952.jpg',
+    'https://i.pinimg.com/550x/55/1a/7a/551a7a8bea201bb7d2ba8adc1302760f.jpg',
+    'https://www.navhindtimes.in/wp-content/uploads/2022/06/1606731360_KK_1.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtXCqErXOOufvU65CDKzSQL9_gTflk1tYZZQ&s',
+    'https://pbs.twimg.com/media/GZXKQU6WgAAgDnd?format=jpg&name=large',
+    'https://i.pinimg.com/550x/9e/4d/9a/9e4d9a80ed803c7351cdcf73cb977048.jpg',
+    'https://static01.nyt.com/images/2018/05/27/arts/27PUTH1/27PUTH1-videoSixteenByNineJumbo1600.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEK-8Ptx5VUBkTVHFGo5aS8IJobj9FvwJnJw&s',
+  ];
+
   final List<String> playlists = ['Liked Songs', 'Rock Classic', 'HeartStrings'];
 
   final List<String> playlistImageUrls = [
@@ -39,11 +63,7 @@ class LibraryScreen extends StatelessWidget {
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyRZrrvULpQkaNDDb2pF-SrdjJkIZodzEyNw&s',
   ];
 
-  final List<String> placeholderImageUrls = [
-    'https://via.placeholder.com/150',
-    'https://via.placeholder.com/150',
-    'https://via.placeholder.com/150',
-  ];
+  final Set<String> likedSongs = {}; // Track liked songs only
 
   @override
   Widget build(BuildContext context) {
@@ -79,39 +99,19 @@ class LibraryScreen extends StatelessWidget {
               labelColor: Colors.white,
               unselectedLabelColor: Colors.grey,
               tabs: [
-                Tab(
-                  child: Text(
-                    'Songs',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    'Albums',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    'Artists',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    'Playlists',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
+                Tab(child: Text('Songs', style: TextStyle(fontSize: 16))),
+                Tab(child: Text('Albums', style: TextStyle(fontSize: 16))),
+                Tab(child: Text('Artists', style: TextStyle(fontSize: 16))),
+                Tab(child: Text('Playlists', style: TextStyle(fontSize: 16))),
               ],
             ),
             Expanded(
               child: TabBarView(
                 children: [
-                  _buildLibrarySectionWithImages(songs, "No songs added yet.", songImageUrls),
-                  _buildLibrarySectionWithImages(albums, "No albums found.", placeholderImageUrls),
-                  _buildLibrarySectionWithImages(artists, "No artists available.", placeholderImageUrls),
-                  _buildLibrarySectionWithImages(playlists, "No playlists created.", playlistImageUrls),
+                  _buildLibrarySectionWithImages(songs, "No songs added yet.", songImageUrls, true),
+                  _buildLibrarySectionWithImages(albums, "No albums found.", albumImageUrls, false),
+                  _buildLibrarySectionWithImages(artists, "No artists available.", artistImageUrls, false),
+                  _buildLibrarySectionWithImages(playlists, "No playlists created.", playlistImageUrls, false),
                 ],
               ),
             ),
@@ -144,47 +144,54 @@ class LibraryScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildLibrarySectionWithImages(List<String> items, String placeholder, List<String> imageUrls) {
+  Widget _buildLibrarySectionWithImages(
+      List<String> items, String placeholder, List<String> imageUrls, bool isSongSection) {
     return items.isNotEmpty
         ? ListView.builder(
       itemCount: items.length,
-      shrinkWrap: true,
-      physics: AlwaysScrollableScrollPhysics(),
       itemBuilder: (context, index) {
-        // Use the provided image URL or fallback to placeholder if out of range
-        String imageUrl = index < imageUrls.length ? imageUrls[index] : placeholderImageUrls[index % placeholderImageUrls.length];
-        return Card(
-          color: Colors.grey[800],
-          margin: EdgeInsets.symmetric(vertical: 8.0),
-          child: ListTile(
-            leading: Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(imageUrl),
-                ),
-              ),
+        return ListTile(
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              imageUrls[index],
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
             ),
-            title: Text(
-              items[index],
-              style: TextStyle(color: Colors.white, fontSize: 22),
-              textAlign: TextAlign.center,
-            ),
-            onTap: () {
-              print("${items[index]} tapped");
-            },
           ),
+          title: Text(
+            items[index],
+            style: TextStyle(color: Colors.white),
+          ),
+          trailing: isSongSection
+              ? IconButton(
+            icon: Icon(
+              likedSongs.contains(items[index])
+                  ? Icons.favorite
+                  : Icons.favorite_border,
+              color: Colors.red,
+            ),
+            onPressed: () {
+              setState(() {
+                if (likedSongs.contains(items[index])) {
+                  likedSongs.remove(items[index]);
+                } else {
+                  likedSongs.add(items[index]);
+                }
+              });
+            },
+          )
+              : null,
         );
       },
     )
         : Center(
       child: Text(
         placeholder,
-        style: TextStyle(color: Colors.white, fontSize: 18),
+        style: TextStyle(color: Colors.white, fontSize: 20),
       ),
     );
   }
 }
+
